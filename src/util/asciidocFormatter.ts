ad0ae9e74a9f5799ee64165028ed51153c16f0ea
George Adams <georgeadams1995@gmail.com>
2022-02-04 10:54:07 +0000

    switch supported-platforms to be asciidoc rather than html

--- src/util/asciidocFormatter.ts
+++ src/util/asciidocFormatter.ts
@@ -1,6 +1,7 @@
 export default function asciidocFormatter() {
   const tables = document.querySelectorAll('table')
   tables.forEach(table => {
+    console.log(table)
     table.className = 'table table-hover py-2'  
   })
 }
