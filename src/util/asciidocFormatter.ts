840ad9cf02e23223c3a9dce59943effa53608503
George Adams <george.adams@microsoft.com>
2022-02-04 10:54:54 +0000

    Update asciidocFormatter.ts

--- src/util/asciidocFormatter.ts
+++ src/util/asciidocFormatter.ts
@@ -1,7 +1,6 @@
 export default function asciidocFormatter() {
   const tables = document.querySelectorAll('table')
   tables.forEach(table => {
-    console.log(table)
     table.className = 'table table-hover py-2'  
   })
 }
