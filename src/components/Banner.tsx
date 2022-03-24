e93ce2bab3c0a18e3068936d583b379c3ee42d5f
George Adams <georgeadams1995@gmail.com>
2022-03-24 13:25:13 +0000

    prep for launch

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className="alert alert-light bg-pink alert-dismissible fade show mb-0 text-center" role="alert">
-        <strong className='p-1'>11th February 2022:</strong>
-            If you have recently picked up the jdk-11.0.14+9 release, please update to jdk-11.0.14.1+1 to pick up 
-            <a href="https://github.com/adoptium/jdk11u/commit/ef5fff53ef1b047c2fca47047fe743689cc2734f"
-            className="alert-link p-1"
+    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+        <strong className='p-1'>22nd March 2022:</strong>
+            The Eclipse Temurin 18 GA Release effort has begun! You can track progress
+            <a href="https://github.com/adoptium/adoptium/issues/131"
+            className="alert-link p-1 text-white"
             >
-                a fix for a regression in the HTTPClient code
+                by platform
+            </a>
+            or
+            <a href="https://github.com/adoptium/adoptium/issues/130"
+            className="alert-link p-1 text-white"
+            >
+                by detailed release checklist
             </a>.
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
