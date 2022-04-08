e59f39c163ab813b405a25d92932808096f5215a
George Adams <georgeadams1995@gmail.com>
2022-04-08 10:26:15 +0100

    Update MarketplaceDownloadTable.tsx

--- src/components/MarketplaceDownloadTable.tsx
+++ src/components/MarketplaceDownloadTable.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby";
+import { Link } from 'gatsby-plugin-react-i18next';
 import { FaDownload } from 'react-icons/fa';
 import { capitalize } from '../util/capitalize';
 import { getImageForVendor, getVendorForDistribution } from '../hooks'
