7873b97a5981238df96c4aad71443f1359933431
Tim Ellison <tellison@users.noreply.github.com>
2022-04-14 21:48:27 +0100

    Fix spacing and align filter text

--- src/pages/temurin/releases.js
+++ src/pages/temurin/releases.js
@@ -27,19 +27,21 @@ const TemurinReleases = () => (
             </div>
             <div className='col-12 col-sm-6 col-md-8'>
               <p className='text-start'>
-                Eclipse Temurin is the open source Java SE build based upon OpenJDK. Temurin is available for a
-                <a href='/supported-platforms'>wide range of platforms</a> and Java SE versions. The latest
-                releases recommended for use in production are listed below, and are regularly
-                <a href='/support'>updated and supported</a> by the Adoptium community.
-                <a href='/migration'>Migration</a> and <a href='/installation'>installation</a> help is
+                Eclipse Temurin is the open source Java SE build based upon OpenJDK. Temurin is available for a 
+                <a href='/supported-platforms'>wide range of platforms</a> and Java SE versions. The latest 
+                releases recommended for use in production are listed below, and are regularly 
+                <a href='/support'>updated and supported</a> by the Adoptium community. 
+                <a href='/migration'>Migration</a> and <a href='/installation'>installation</a> help is 
                 available in the <a href='/docs'>documentation section</a>.
-              </p>
-              <p className='text-start'>
-                Use the drop down boxes to filter the list of current releases.
               </p>
             </div>
           </div>
         </div>
+      </div>
+      <div className='row align-items-center pt-3'>
+        <p className='text-center'>
+          Use the drop down boxes to filter the list of current releases.
+        </p>
       </div>
       <DownloadDropdowns updaterAction={loadLatestAssets} marketplace={false} Table={DownloadTable} />
       <ChecksumModal />
