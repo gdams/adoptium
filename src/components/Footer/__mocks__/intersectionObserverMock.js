db4a620c599600911ff319b34669c3cd97ae6c2a
George Adams <georgeadams1995@gmail.com>
2022-04-20 15:32:04 -0500

    add more tests

---
+++ src/components/Footer/__mocks__/intersectionObserverMock.js
@@ -0,0 +1,17 @@
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  observe() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+
+  disconnect() {
+    return null;
+  }
+};
+
+// window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
