fcef94971a747e251ecbc71d3c9586ae5970a807
George Adams <georgeadams1995@gmail.com>
2022-06-06 14:50:54 +0100

    linter fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
-import { SSRProvider } from '@react-aria/ssr'; 
+import { SSRProvider } from '@react-aria/ssr'
 
 import Navbar from './Navbar'
 import Banner from './Banner'
