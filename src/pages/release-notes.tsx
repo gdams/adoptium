c5d0b00a6c86429f6fa0e1939a0841172dd34222
Bethany Nicolle Griggs <bethanyngriggs@gmail.com>
2023-01-30 13:18:24 +0000

    fixup! add trans tag to Release Notes title
    Co-authored-by: George Adams <george.adams@microsoft.com>

--- src/pages/release-notes.tsx
+++ src/pages/release-notes.tsx
@@ -11,7 +11,7 @@ const ReleaseNotesPage = ({ data }) => (
     <section className='py-5 text-center container'>
       <div className='row py-lg-5'>
         <div className='col-lg-8 col-md-8 mx-auto'>
-          <h1 className='fw-light'>Release Notes</h1>
+          <h1 className='fw-light'><Trans>Release Notes</Trans></h1>
         </div>
         <ReleaseNotesRender />
       </div>
