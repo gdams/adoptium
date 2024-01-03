33e57609b6e3aed4110cb33200610d6c2ca65a9e
Xavier FACQ <xavier.facq@free.fr>
2024-01-03 16:52:17 +0100

    Fix pb with Found errors in [ts-standard] linter!

--- jest-canvas-mock.ts
+++ jest-canvas-mock.ts
@@ -1,9 +1,9 @@
 // https://github.com/vitest-dev/vitest/discussions/395
 
 import 'jest-canvas-mock';
-// import { setupJestCanvasMock } from 'jest-canvas-mock';
+import { setupJestCanvasMock } from 'jest-canvas-mock';
 
-// beforeEach(() => {
-//   jest.resetAllMocks();
-//   setupJestCanvasMock();
-// });
+beforeEach(() => {
+  jest.resetAllMocks();
+  setupJestCanvasMock();
+});
