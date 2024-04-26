4365b1ac9a2f921b9638eebc50e224b18b7bc8ed
Xavier FACQ <xavier.facq@free.fr>
2024-04-26 13:48:08 +0200

    Change . by :

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
    return (
      <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
        <strong className='p-1'>Case Study: Building the World's Most Secure OpenJDK Distribution</strong><br/>
-       Find out how the Eclipse Foundation and Adoptium Working Group are pioneering software supply chain security with Eclipse Temurin. 
+       Find out how the Eclipse Foundation and Adoptium Working Group are pioneering software supply chain security with Eclipse Temurin: 
        <a className='alert-link p-1 text-white' href="https://outreach.eclipse.foundation/adoptium-temurin-supply-chain-security?utm_campaign=Temurin%20Case%20Study&utm_source=website&utm_medium=adoptium%20docs">Download now</a>
        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
      </div>
