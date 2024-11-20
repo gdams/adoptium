a7d5dcb452f506fb3384ee15e135b31f4eb2fa99
Xavier FACQ <xavier.facq@free.fr>
2024-11-20 17:19:29 +0100

    Remove unused import

--- src/components/MarketplaceDownloadTable/__tests__/MarketplaceDownloadTable.test.tsx
+++ src/components/MarketplaceDownloadTable/__tests__/MarketplaceDownloadTable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest'
 import { createRandomMarketplaceRelease } from '../../../__fixtures__/hooks';
 import MarketplaceDownloadTable from '..';
